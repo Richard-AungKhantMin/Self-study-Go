ee3570191e753cfa722982ea5352d1fcbf9be21d
amin <aungkhantmin.dev@gmail.com>
2024-09-24 15:10:34 +0300

    ka li

--- Go packages practice/handling files/create write and errors/p1.go
+++ Go packages practice/handling files/create write and errors/p1.go
@@ -1,3 +1,7 @@
+//os.Create
+//os.ReadFile
+//io.WriteString
+
 package main
 
 import (
@@ -7,20 +11,38 @@ import (
 )
 
 func main() {
-	fmt.Println("Start")
+	fmt.Println("1. The program starts here.")
+	//prints here
 
 	content := "Chan Myint is one of the students from Gritlab"
 
 	file, err := os.Create("./new_file.txt")
 	isErrNill(err)
+	fmt.Println("2. ", file)
+	//prints here
 
 	length, err := io.WriteString(file, content)
 	isErrNill(err)
-	fmt.Println("The length of the content is:", length)
+	fmt.Println("3. The length of the content is:", length)
+	//prints here
 
 	each, err := os.ReadFile("./new_file.txt")
 	isErrNill(err)
-	fmt.Println(string(each))
+	fmt.Println("4. ", string(each))
+	// prints here
+
+	new, err := os.Create("./new_file.txt")
+	isErrNill(err)
+	fmt.Println("5. ", new)
+	//prints here
+
+	new1, err := os.ReadFile("./new_file.txt")
+	isErrNill(err)
+	fmt.Println("6. ", new1)
+	//prints here
+
+	fmt.Println("7. ", string(each))
+	// prints here
 }
 
 func isErrNill(err error) {
